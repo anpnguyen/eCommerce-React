6e2ce9983291028ac6bab84cb5deb59323e79945
PleatherShaman <shamanwebsolutions@gmail.com>
2019-09-19 16:14:26 +1000

    clean up code

--- client/src/components/StripeContainer.js
+++ client/src/components/StripeContainer.js
@@ -1,36 +1,23 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-import {
-  CardElement,
-  injectStripe,
-  ReactStripeElements
-} from "react-stripe-elements";
+import { CardElement, injectStripe } from "react-stripe-elements";
 import "./stripe.css";
 
 const StripeContainer = props => {
   const { stripe } = props;
-  const [cart, setCart] = useState([{ item: "bananna", quantity: 0 }]);
-  const [loading, isLoading] = useState(false);
-
-  const [user, setUser] = useState({ name: "", email: "", address: "" });
-
-  const prices = {
-    apple: 150,
-    orange: 250
-  };
-
-  const sku = {
-    tshirt: "sku_FnJ7E5EEavmbWd",
-    ebook: "sku_FmxE14XcellRYv"
-  };
+  const [cart, setCart] = useState([
+    { item: "T-Shirt", sku: "sku_FnJ7E5EEavmbWd", quantity: 0 },
+    { item: "e-Book", sku: "sku_FmxE14XcellRYv", quantity: 0 }
+  ]);
 
   const axiosHeader = {
-    "Content-Type": "application/json"    
+    "Content-Type": "application/json"
   };
 
   const handleCartChange = e => {
     e.preventDefault();
+
     let updated = cart.find(cartItem => cartItem.item === e.target.name);
     updated.quantity += parseInt(e.target.value);
     setCart([...cart, updated]);
@@ -41,16 +28,12 @@ const StripeContainer = props => {
 
     // this token allows you to store the suers name that is linked to the cc, but not the cc number itseld
     const { token } = await stripe.createToken();
-    // {name:'An', email:"aa@aa.com"}
-    console.log(token);
-
-    const totalPrice = 500;
 
     const order = {
       currency: "aud",
       items: [
-        { type: "sku", parent: "sku_FnJ7E5EEavmbWd", quantity: 2 },
-        { type: "sku", parent: "sku_Fmy60bnUBvxkrD", quantity: 5 }
+        { type: "sku", parent: cart[0].sku, quantity: cart[0].quantity },
+        { type: "sku", parent: cart[1].sku, quantity: cart[1].quantity }
       ],
 
       email: "aa@aa.com",
@@ -64,55 +47,61 @@ const StripeContainer = props => {
           state: "CA"
         }
       }
-      // selected_shipping_method: 'flat_fee',
-      // shipping_methods: [
-      //   {
-      //     id: "flat_fee",
-      //     amount: 0,
-      //     currency: "aud",
-      //     delivery_estimate: null,
-      //     description: "Free shipping"
-      //   }
-      // ]
     };
 
-    const response = await axios.post(
-      "/order",
-      JSON.stringify({
-        order: order,
-        source: token.id
-      }),
-      { headers: axiosHeader }
-    );
+    try {
+      const response = await axios.post(
+        "/order",
+        JSON.stringify({
+          order: order,
+          source: token.id
+        }),
+        { headers: axiosHeader }
+      );
 
-    console.log(response);
+      alert(
+        `Your order of ${response.data.amount} was successfully processed `
+      );
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
     <div>
-      <h1>
-        this is my form component
-        <div>
-          <h3>this is where the items will go</h3>
-          <button name="bananna" value={-1} onClick={handleCartChange}>
+      <h1>this is my form component</h1>
+      <div>
+        <h3>this is where the items will go</h3>
+
+        <div className="">
+          <button name="T-Shirt" value={-1} onClick={handleCartChange}>
             -
           </button>
-          <button>{cart[0].item}</button>
-          <button>{cart[0].quantity}</button>
-          <button name="bananna" value={1} onClick={handleCartChange}>
+          {cart[0].item}
+          {cart[0].quantity}
+          $10
+          <button name="T-Shirt" value={1} onClick={handleCartChange}>
             +
           </button>
         </div>
-        <form action="" className="container" onSubmit={handleCartSubmit}>
-          {/* <label htmlFor="">Name</label>
-          <input type="text" /> */}
-          {/* <label htmlFor="">CC number</label>
-          <input type="text" /> */}
-          <CardElement />
 
-          <button>Submit</button>
-        </form>
-      </h1>
+        <div className="">
+          <button name="e-Book" value={-1} onClick={handleCartChange}>
+            -
+          </button>
+          {cart[1].item}
+          {cart[1].quantity}
+          $15
+          <button name="e-Book" value={1} onClick={handleCartChange}>
+            +
+          </button>
+        </div>
+      </div>
+      <form action="" className="container" onSubmit={handleCartSubmit}>
+        <CardElement />
+
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
